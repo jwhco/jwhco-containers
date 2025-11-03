c285f788e4bc20150a84f55cbdafd578a41427b8
Justin Hitt <justin@jwhco.org>
2025-11-03 16:15:31 -0500

    trying to get this to work

---
+++ obsidian/obsidian-install.sh
@@ -0,0 +1,11 @@
+apt-get update && \
+apt-get install -y xfce4 xfce4-goodies tigervnc-standalone-server && \
+# Replace with the command to install Obsidian
+snap install obsidian --classic && \
+
+# Set up VNC password (requires a tool like `vncpasswd` to set up)
+mkdir -p /root/.vnc && \
+echo "monkey23" | vncpasswd -f > /root/.vnc/passwd && \
+chmod 600 /root/.vnc/passwd && \
+# Start VNC server
+vncserver :1 -geometry 1280x800 -depth 24
