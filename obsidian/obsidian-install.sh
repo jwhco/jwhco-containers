794d03c187a962e9586ac4d5217fac34c9323b96
Justin Hitt <justin@jwhco.org>
2026-02-06 12:50:06 -0500

    tuning required packages

--- obsidian/obsidian-install.sh
+++ obsidian/obsidian-install.sh
@@ -5,7 +5,7 @@ snap install obsidian --classic && \
 
 # Set up VNC password (requires a tool like `vncpasswd` to set up)
 mkdir -p /root/.vnc && \
-echo "monkey23" | vncpasswd -f > /root/.vnc/passwd && \
+echo "InSeRtPaSsWoRd" | vncpasswd -f > /root/.vnc/passwd && \
 chmod 600 /root/.vnc/passwd && \
 # Start VNC server
 vncserver :1 -geometry 1280x800 -depth 24
